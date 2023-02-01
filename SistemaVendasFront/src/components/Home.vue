bd43451f36f8547ba30fef52d661c45e5b4c3a3a
giovannibenner <giovanni.guilioti@benner.com.br>
2023-02-01 14:11:44 -0300

    card example

--- SistemaVendasFront/src/components/Home.vue
+++ SistemaVendasFront/src/components/Home.vue
@@ -1,3 +1,13 @@
 <template>
-  <h1 style="width: 100%;">Seja Bem vindo ao sistema</h1>
+  <div>
+    <h1 style="width: 100%;">Seja Bem vindo ao sistema</h1>
+    <div class="card">
+      <div class="card-body">
+        This is some text within a card body.
+      </div>
+    </div>
+  </div>
 </template>
+<style scoped>
+
+</style>
